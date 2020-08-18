a67c678a202ca23e05297f069866eb36bda24a0d
YarbashSaleem <yarbashsaleem98@gmail.com>
2020-08-18 17:08:35 +0530

    Updates

---
+++ precache-manifest.0cb0c2d96579e5be0e9e3b92d61bab05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65893f8ed558ac74ae050e20d8d3b831",
+    "url": "/agrivator/index.html"
+  },
+  {
+    "revision": "2379ec98ac43192478e2",
+    "url": "/agrivator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65f4888de5f1aa575207",
+    "url": "/agrivator/static/js/2.6bd80cef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/agrivator/static/js/2.6bd80cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2379ec98ac43192478e2",
+    "url": "/agrivator/static/js/main.a5848d29.chunk.js"
+  },
+  {
+    "revision": "ae9cc243608f87e07708",
+    "url": "/agrivator/static/js/runtime-main.35d6ef65.js"
+  }
+]);
