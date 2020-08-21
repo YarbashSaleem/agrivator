369accfcb901c535d516b14cebe58cbbb671a5f6
YarbashSaleem <yarbashsaleem98@gmail.com>
2020-08-21 20:28:49 +0530

    Updates

---
+++ precache-manifest.ac8afe2740d0b93f23d387feefd78e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3732ec475e29e225e9c4ac05ac1f862b",
+    "url": "/agrivator/index.html"
+  },
+  {
+    "revision": "295406282a1f9d7157c2",
+    "url": "/agrivator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0feb7ae63d9d92ac7148",
+    "url": "/agrivator/static/js/2.15979874.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/agrivator/static/js/2.15979874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295406282a1f9d7157c2",
+    "url": "/agrivator/static/js/main.b523e0a4.chunk.js"
+  },
+  {
+    "revision": "ae9cc243608f87e07708",
+    "url": "/agrivator/static/js/runtime-main.35d6ef65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e33cfb9d20743305403b1892d3942b48.js"
+  "/agrivator/precache-manifest.ac8afe2740d0b93f23d387feefd78e5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/agrivator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
